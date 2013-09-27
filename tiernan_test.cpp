d759d8333f71ee8d89383a3208c48803322c7f6e
Jonathan Bohren <jbo@jhu.edu>
2013-09-27 19:21:43 -0400

    adding positive test

--- tiernan_test.cpp
+++ tiernan_test.cpp
@@ -1,6 +1,22 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/tiernan_all_cycles.hpp>
 #include <boost/graph/labeled_graph.hpp>
+#include <boost/graph/directed_graph.hpp>
+
+typedef 
+boost::directed_graph
+< boost::no_property,
+  boost::no_property> 
+graph_directed;
+
+typedef 
+boost::adjacency_list
+< boost::vecS,
+  boost::vecS,
+  boost::bidirectionalS, 
+  boost::property<boost::vertex_index_t, std::size_t>, 
+  boost::no_property> 
+graph;
 
 struct vprop {
 };
@@ -13,15 +29,19 @@ boost::adjacency_list
 < boost::vecS,
   boost::vecS,
   boost::bidirectionalS, 
-  boost::property<boost::vertex_index_t, std::size_t>, 
-  boost::no_property> 
-graph;
+  vprop,
+  eprop> 
+graph_bundled;
 
 
 int main(int argc, char** argv) {
 
-  // This fails for some reason
+  // This works
+  BOOST_CONCEPT_ASSERT(( boost::VertexIndexGraphConcept<graph_directed> ));
+  
+  // These fail for some reason
   BOOST_CONCEPT_ASSERT(( boost::VertexIndexGraphConcept<graph> ));
+  BOOST_CONCEPT_ASSERT(( boost::VertexIndexGraphConcept<graph_bundled> ));
 
   return 0;
 }
