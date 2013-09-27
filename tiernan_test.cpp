b1a5bce4a995206c8d4fa748a7636754b01e4d82
Jonathan Bohren <jbo@jhu.edu>
2013-09-27 19:04:27 -0400

    initial commit

---
+++ tiernan_test.cpp
@@ -0,0 +1,27 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/tiernan_all_cycles.hpp>
+#include <boost/graph/labeled_graph.hpp>
+
+struct vprop {
+};
+
+struct eprop {
+};
+
+typedef 
+boost::adjacency_list
+< boost::vecS,
+  boost::vecS,
+  boost::bidirectionalS, 
+  boost::property<boost::vertex_index_t, std::size_t>, 
+  boost::no_property> 
+graph;
+
+
+int main(int argc, char** argv) {
+
+
+  BOOST_CONCEPT_ASSERT(( boost::VertexIndexGraphConcept<graph> ));
+
+  return 0;
+}
